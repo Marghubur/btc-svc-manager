157786e0e717d69a6b6b98ed093f88dd0b6e7db0
BottomHalf <istiyaq.mi9@gmail.com>
2026-06-07 21:45:19 +0530

    resolved code error

--- src/main/java/bt/conference/controller/LivekitController.java
+++ src/main/java/bt/conference/controller/LivekitController.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/conference/")
 public class LivekitController {
